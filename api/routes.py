50873f4ff0d8a58c6b77096379e06c649e3689d1
Robin-das-raam <robindasraam@gmail.com>
2025-07-23 18:30:52 +0600

    Added file uploading folder and functionalities will be added later

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+import os
+import shutil
+import uuid
+from pydantic import BaseModel
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import FileResponse
+
 
 router = APIRouter()
 
+UPLOAD_DIR = "/home/robinpc/Desktop/Fastapi_prac/backend_ci_cd_prac/uploads"
+OUTPUT_DIR = "/home/robinpc/Desktop/Fastapi_prac/backend_ci_cd_prac/output"
+
+os.makedirs(UPLOAD_DIR, exist_ok = True)
+os.makedirs(OUTPUT_DIR, exist_ok = True)
+
 @router.get("/")
 def root():
-    return {"message": "Backend CI/CD Practice Running"}
+    return {"message": "Backend CI/CD Practice Running . Moving ahead"}
+
+
+# @router.post("/upload_video")
